71884ef082c2f1111358f546a228000f3ec87b25
Nicklas Larsen <nicklas@nila.io>
2017-09-14 19:44:01 +0200

    refactor create reminder for promises

--- controllers/botController.js
+++ controllers/botController.js
@@ -4,6 +4,7 @@ const cron = require('node-cron');
 const db = require('../util/db');
 const reminderUtil = require('../util/reminder');
 const cronUtil = require('../util/cron');
+const reminderController = require('./reminderController');
 var reminders = {};
 
 module.exports = {
@@ -138,93 +139,114 @@ module.exports = {
         var reminder = {};
         var msgText = msg.text.split(' ');
 
-        if(msgText.length <= 1) {
-            reply.text('In order to set a reminder you have to put a day, time,' +
-            'interval and name for the reminder.\n' +
-            'Like this:\n\n' +
-            'Today 15:00 1 minute Create new reminder!');
-            return;
-        }
-        var reminderDay = msgText[1].toLowerCase();
-
-
-        /*
-        ** Split message into arguments and validate arguments
-        */
-
-        // Validate time input to a valid hour between 00:00 to 23:59
-        if(/^([01]\d|2[0-3]):?([0-5]\d)$/.test(msgText[2])) {
-            var reminderTime = msgText[2].split(':');
-            reminder.time = {
-                hour: reminderTime[0].replace(/^0/, ''),
-                minute: reminderTime[1].replace(/^0/, '')
-            };
-        } else {
-            reply.text('You didn\'t enter a correct time of day. Please try again.');
-            return;
-        }
-
-        // Validate interval input to a number between 1 and 59
-        if(/^[1-5]?[0-9]$/.test(msgText[3])) {
-            reminder.interval = msgText[3];
-        } else {
-            reply.text('The interval has to be a number within 1 and 59 minutes. Please try again');
-            return;
-        }
-
+        reminder.day = msgText[1].toLowerCase();
+        var reminderTime = msgText[2].split(':');
+        reminder.hour = reminderTime[0];
+        reminder.minute = reminderTime[1];
+        reminder.interval = msgText[3];
         reminder.name = msg.text.split(/minutes? /)[1];
         reminder.active = true;
         reminder.owner = msg.from.id;
 
-        // Identify and validate schedule for reminder
-        if(reminderDay === 'today'){
-            reminder.day = '*';
-            reminder.recurring = false;
-        } else if(reminderDay === 'daily') {
-            reminder.day = '*',
-            reminder.recurring = true;
-        } else if(/\b((mon|tues|wed(nes)?|thur(s)?|fri|sat(ur)?|sun)(days?)?)\b/.test(reminderDay)) {
-            // Check if plural of days and set recurring to match
-            if(reminderDay.charAt(reminderDay.length - 1) === 's') {
-                reminder.day =  reminderDay.substr(0, 3);// remove last letter of the string
-                reminder.recurring = true;
-            } else {
-                reminder.day = reminderDay.substr(0, 3);
-                reminder.recurring = false;
-            }
-        } else {
-            reply.text('You didn\'t enter a valid date, please try again');
-            return;
-        }
+        reminderController.create(reminder, (reminder) => {
+            reply.text(reminder.name);
+        }).then(() => {
+            reply.text('Your reminder has been created!');
+        }).catch((err) => {
+            console.error(err);
+        });
 
-        // Combine some arguments into valid cron string
-        var cronString = reminder.time.minute + ' ' + reminder.time.hour + ' ' + '* * ' + reminder.day;
 
-        /*
-        ** Use arguments to create valid cron job
-        */
-
-        if(cronUtil.validate(cronString)) {
-            reminder.cronString = cronString;
-
-            // Persist object without cron task to database
-            db.get().collection('reminders').insert(reminder, (err, result) => {
-                    if(err) {
-                        reply.text('An error happened while saving the reminder. Please try again.');
-                        return;
-                    };
-
-                    // Create cron job based on valid cron string
-                    reminder.schedule = cronUtil.create(cronString, reminder, msg.from.id);
-
-                    // Add reminder to reminders if everything is successful
-                    reminderUtil.create(msg.from.id, reminder);
-                    reply.text('Your reminder has been saved!');
-                    return;
-            });
-        } else {
-            // TODO: Add better error handling
-            reply.text('It seems there was an error with the format. Please try again');
-        }
+        // var reminder = {};
+        // var msgText = msg.text.split(' ');
+        //
+        // if(msgText.length <= 1) {
+        //     reply.text('In order to set a reminder you have to put a day, time,' +
+        //     'interval and name for the reminder.\n' +
+        //     'Like this:\n\n' +
+        //     'Today 15:00 1 minute Create new reminder!');
+        //     return;
+        // }
+        // var reminderDay = msgText[1].toLowerCase();
+        //
+        //
+        // /*
+        // ** Split message into arguments and validate arguments
+        // */
+        //
+        // // Validate time input to a valid hour between 00:00 to 23:59
+        // if(/^([01]\d|2[0-3]):?([0-5]\d)$/.test(msgText[2])) {
+        //     var reminderTime = msgText[2].split(':');
+        //     reminder.time = {
+        //         hour: reminderTime[0].replace(/^0/, ''),
+        //         minute: reminderTime[1].replace(/^0/, '')
+        //     };
+        // } else {
+        //     reply.text('You didn\'t enter a correct time of day. Please try again.');
+        //     return;
+        // }
+        //
+        // // Validate interval input to a number between 1 and 59
+        // if(/^[1-5]?[0-9]$/.test(msgText[3])) {
+        //     reminder.interval = msgText[3];
+        // } else {
+        //     reply.text('The interval has to be a number within 1 and 59 minutes. Please try again');
+        //     return;
+        // }
+        //
+        // reminder.name = msg.text.split(/minutes? /)[1];
+        // reminder.active = true;
+        // reminder.owner = msg.from.id;
+        //
+        // // Identify and validate schedule for reminder
+        // if(reminderDay === 'today'){
+        //     reminder.day = '*';
+        //     reminder.recurring = false;
+        // } else if(reminderDay === 'daily') {
+        //     reminder.day = '*',
+        //     reminder.recurring = true;
+        // } else if(/\b((mon|tues|wed(nes)?|thur(s)?|fri|sat(ur)?|sun)(days?)?)\b/.test(reminderDay)) {
+        //     // Check if plural of days and set recurring to match
+        //     if(reminderDay.charAt(reminderDay.length - 1) === 's') {
+        //         reminder.day =  reminderDay.substr(0, 3);// remove last letter of the string
+        //         reminder.recurring = true;
+        //     } else {
+        //         reminder.day = reminderDay.substr(0, 3);
+        //         reminder.recurring = false;
+        //     }
+        // } else {
+        //     reply.text('You didn\'t enter a valid date, please try again');
+        //     return;
+        // }
+        //
+        // // Combine some arguments into valid cron string
+        // var cronString = reminder.time.minute + ' ' + reminder.time.hour + ' ' + '* * ' + reminder.day;
+        //
+        // /*
+        // ** Use arguments to create valid cron job
+        // */
+        //
+        // if(cronUtil.validate(cronString)) {
+        //     reminder.cronString = cronString;
+        //
+        //     // Persist object without cron task to database
+        //     db.get().collection('reminders').insert(reminder, (err, result) => {
+        //             if(err) {
+        //                 reply.text('An error happened while saving the reminder. Please try again.');
+        //                 return;
+        //             };
+        //
+        //             // Create cron job based on valid cron string
+        //             reminder.schedule = cronUtil.create(cronString, reminder, msg.from.id);
+        //
+        //             // Add reminder to reminders if everything is successful
+        //             reminderUtil.create(msg.from.id, reminder);
+        //             reply.text('Your reminder has been saved!');
+        //             return;
+        //     });
+        // } else {
+        //     // TODO: Add better error handling
+        //     reply.text('It seems there was an error with the format. Please try again');
+        // }
     }
 }
---
+++ controllers/reminderController.js
@@ -0,0 +1,191 @@
+"use strict";
+
+const cronUtil = require('../util/cron');
+const reminderUtil = require('../util/reminder');
+const db = require('../util/db');
+const Promise = require('bluebird');
+const cron = require('node-cron');
+
+
+
+module.exports = {
+    /**
+     * Creates a reminder with a cron job and persists it to the database
+     * @param  {Object}   reminder  Must contain: {
+     *                                  name: String,
+     *                                  owner: String,
+     *                                  hour: String,
+     *                                  minute: String,
+     *                                  interval: String
+     *                              }
+     *
+     * @param  {Function} callback  Callback to be executed by cron job
+     *
+     * @return {Promise}            Returns validated reminder
+     */
+    create: (reminder, callback) => {
+        if(!reminder) {
+            Promise.reject(new Error('Reminder was undefined or not passed'));
+        }
+
+        const validateNameAndTime = (reminder) => {
+
+            return new Promise((resolve, reject) => {
+                console.log(reminder);
+
+                if(!reminder.name) reject(new Error('The reminder needs to have a name'));
+
+                if(!reminder.hour) reject(new Error('Hour of reminder was not defined'));
+
+                if(!reminder.minute) reject(new Error('Minute of reminder was not defined'));
+
+                if(!/\b(2[0-3]|[1]?[0-9])\b/.test(reminder.hour)) reject(new Error('Hour is not valid. Must be a value between 0 and 23'));
+
+                if(!/\b(5[0-9]|[0-5]?[0-9])\b/.test(reminder.minute)) reject(new Error('Minute is not valid. Must be a value between 0 and 59'));
+
+
+                reminder.time = {
+                    hour: reminder.hour.replace(/^0/, ''),
+                    minute: reminder.minute.replace(/^0/, '')
+                };
+
+                resolve(reminder);
+            });
+        }
+        const validateDay = (reminder) => {
+            return new Promise((resolve, reject) => {
+                if(!reminder.day) reject(new Error('Day has not been defined'));
+
+                if(reminder.day === 'today'){
+                    reminder.day = '*';
+                    reminder.recurring = false;
+                } else if(reminder.day === 'daily') {
+                    reminder.day = '*',
+                    reminder.recurring = true;
+                } else if(/\b((mon|tues|wed(nes)?|thur(s)?|fri|sat(ur)?|sun)(days?)?)\b/.test(reminder.day)) {
+                    // Check if plural of days and set recurring to match
+                    if(reminder.day.charAt(reminder.day.length - 1) === 's') {
+                        reminder.day =  reminder.day.substr(0, 3);// remove last letter of the string
+                        reminder.recurring = true;
+                    } else {
+                        reminder.day = reminder.day.substr(0, 3);
+                        reminder.recurring = false;
+                    }
+                } else {
+                    reject(new Error('Day was not valid. Must be a week day, today, or daily.'));
+                }
+
+                resolve(reminder);
+            });
+        }
+        const createCronString = (reminder) => {
+            var cronString = reminder.time.minute + ' ' + reminder.time.hour + ' ' + '* * ' + reminder.day;
+
+            if(cron.validate(cronString)) {
+                reminder.cronString = cronString;
+            } else {
+                throw new Error('Cron string was not valid.');
+            }
+
+            return reminder;
+        }
+        const persistReminder = (reminder) => {
+            return db.get().collection('reminders').insertAsync(reminder).then(() => {
+                reminderUtil.create(reminder.owner, reminder);
+                return reminder;
+            }).catch((err) => {
+                return new Error('Error while inserting reminder in database');
+            });
+        }
+        const createCronAndUpdateReminder = (reminder) => {
+            return new Promise((resolve, reject) => {
+
+                reminder.schedule = cron.schedule(reminder.cronString, () => {
+                    db.get().collection('reminders')
+                            .findOneAsync({owner:reminder.owner, name: reminder.name})
+                            .then((scheduleReminder) => {
+                                var activeReminder = reminderUtil.findOne(scheduleReminder.owner, scheduleReminder.name);
+
+                                if(!activeReminder.active && activeReminder.recurring) {
+                                    activeReminder.active = true;
+                                    db.get().collection('reminders')
+                                            .updateOneAync({
+                                                owner:activeReminder.owner,
+                                                name:activeReminder.name
+                                            }, {$set:{active:true}})
+                                            .then(() => {
+                                                reminderUtil.updateOne(activeReminder.owner,
+                                                                       activeReminder.name,
+                                                                       activeReminder);
+                                                return;
+                                            })
+                                } else if(!activeReminder.active && !activeReminder.recurring) {
+                                    activeReminder.schedule.destroy();
+                                    reminderUtil.updateOne(activeReminder.owner,
+                                                           activeReminder.name,
+                                                           activeReminder);
+                                    return;
+                                }
+
+                                callback(activeReminder);
+
+                                var intervalCronString = '*/' + activeReminder.interval + ' * * * *';
+                                if(cron.validate(intervalCronString)) {
+                                    activeReminder.intervalSchedule = cron.schedule(intervalCronString, () => {
+                                        callback(activeReminder);
+                                    });
+
+                                    reminderUtil.updateOne(activeReminder.owner,
+                                                           activeReminder.name,
+                                                           activeReminder);
+                                } else {
+                                    activeReminder.intervalSchedule = cron.schedule('*/5 * * * *', () => {
+                                        callback(activeReminder);
+                                    });
+
+                                    reminderUtil.updateOne(activeReminder.owner,
+                                                           activeReminder.name,
+                                                           activeReminder);
+                                }
+
+                                if(!activeReminder.recurring) {
+                                    activeReminder.active = false;
+                                    activeReminder.schedule.destroy();
+                                    db.get().collection('reminders')
+                                            .updateOneAsync({
+                                                owner:activeReminder.owner,
+                                                name:activeReminder.name
+                                            }, {$set:{active:true}})
+                                            .then(() => {
+                                                reminderUtil.updateOne(activeReminder.owner,
+                                                                       activeReminder.name,
+                                                                       activeReminder);
+                                            });
+                                    return;
+                                }
+
+                            });
+                });
+
+                reminderUtil.updateOne(reminder.owner, reminder.name, reminder);
+                console.log(reminder);
+                resolve(reminder);
+            });
+        }
+
+        return validateNameAndTime(reminder).then(validateDay)
+                .then(createCronString)
+                .then(persistReminder)
+                .then(createCronAndUpdateReminder);
+
+    },
+    delete: (id) => {
+
+    },
+    update: (id, reminder) => {
+
+    },
+    get: (id) => {
+
+    }
+}
